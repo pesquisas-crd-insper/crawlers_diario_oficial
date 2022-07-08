0148b89b4eb73b7ee224e93681ef8578dcfe7a8b
saylon1986 <saylon.pereira@gmail.com>
2022-07-08 15:16:18 -0300

    coletor v2

--- TRF1/Diario_TRF1_justica_coleta.py
+++ TRF1/Diario_TRF1_justica_coleta.py
@@ -74,12 +74,75 @@ def colet_2020_adiante(ano):
 			# z= input("")
 
 
+def colet_2015_2020(ano):
+
+	dir_path = str(os.path.dirname(os.path.realpath(__file__)))
+
+	for k in range(1252,1350):#26653):
+		url = 'https://sistemas.trf1.jus.br/edj/handle/123/'+str(k)
+		print("--------------------")
+		print()
+		print(url)
+		try:
+			response = urllib.request.urlopen(url)
+			soup = BeautifulSoup(response, 'html.parser')
+			# print(soup)
+			# z = input("")
+			base = 'https://sistemas.trf1.jus.br'
+			count = 0
+			for link in soup.find_all('a'):
+				nome_link = link.get('href')
+				if "Caderno" in str(nome_link) and count == 0 and ano in str(nome_link):
+					print(nome_link)
+					count = 1
+					# z= input("")
+					link_final = base+nome_link
+					partes = nome_link.split("/")
+					nome_arquivo, nada = partes[-1].split('.pdf')
+					nome_parseado = nome_arquivo.split("_")
+					estado = nome_parseado [2]
+					data = nome_parseado[3]
+					itens_data = data.split("-")
+					data_ajust = str(itens_data[2])+"-"+itens_data[1]+"-"+itens_data[0]
+
+					if "JUD" in nome_arquivo:
+						print("-------------")
+						print()
+						print(data_ajust)
+						print(nome_arquivo)
+						print()
+						print("-------------")
+						path_final = dir_path + f'\Diarios_TRF1_'+str(estado)+"_"+str(itens_data[0])+'\\'+str(data_ajust)
+						Path(path_final).mkdir(parents=True, exist_ok=True)
+											
+						response_2 = urllib.request.urlopen(link_final, timeout = 30)
+						file = open(path_final+"/"+nome_arquivo+".pdf", "wb")
+						file.write(response_2.read())
+						time.sleep(2)
+						file.close()
+				# else:
+					# print("link que não remete ao documento ou que não pertence ao ano buscado")
+		except:
+			pass
+			# print("não possui link válido")
+
+
 
 
 def Baixar_diarios(ano):
 
-	if int(ano) > 2019:
+	if int(ano) > 2020:
 		colet_2020_adiante(ano)
+	elif int(ano) == 2020:
+		colet_2020_adiante(ano)
+		colet_2015_2020(ano)
+	elif 2015 < int(ano) < 2020:
+		colet_2015_2020(ano)
+	elif int(ano) == 2015:
+		colet_2015_2020(ano)
+		print("e o outro abaixo")
+	elif int(ano) < 2015:
+		print("ainda vai ser feito!")
 
 
 def main():
